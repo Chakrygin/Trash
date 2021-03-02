f52780b0a862edc6ab6dc2fc554bbd14d53b3656
Igor Chakrygin <igor@chakrygin.ru>
2021-03-03 02:54:55 +0300

    ...

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tzs = TimeZoneInfo.GetSystemTimeZones();
+
+            foreach (var tz in tzs)
+            {
+                Console.WriteLine($"{tz.DisplayName} - {tz.StandardName} - {tz.BaseUtcOffset}");
+            }
+        }
+    }
+}
